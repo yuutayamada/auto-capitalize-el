f69e476d93499c22b8406c1df153f5e37a4f9939
Yuta Yamada <cokesboy@gmail.com>
2013-09-06 11:05:15 -0400

    Use space instead of TABs

--- auto-capitalize.el
+++ auto-capitalize.el
@@ -43,19 +43,19 @@
 ;; To install auto-capitalize.el, copy it to a `load-path' directory,
 ;; `M-x byte-compile-file' it, and add this to your
 ;; site-lisp/default.el or ~/.emacs file:
-;; 	(autoload 'auto-capitalize-mode "auto-capitalize"
-;; 	  "Toggle `auto-capitalize' minor mode in this buffer." t)
-;; 	(autoload 'turn-on-auto-capitalize-mode "auto-capitalize"
-;; 	  "Turn on `auto-capitalize' minor mode in this buffer." t)
-;; 	(autoload 'enable-auto-capitalize-mode "auto-capitalize"
-;; 	  "Enable `auto-capitalize' minor mode in this buffer." t)
+;; (autoload 'auto-capitalize-mode "auto-capitalize"
+;;   "Toggle `auto-capitalize' minor mode in this buffer." t)
+;; (autoload 'turn-on-auto-capitalize-mode "auto-capitalize"
+;;   "Turn on `auto-capitalize' minor mode in this buffer." t)
+;; (autoload 'enable-auto-capitalize-mode "auto-capitalize"
+;;   "Enable `auto-capitalize' minor mode in this buffer." t)
 ;; 
 ;; To turn on (unconditional) capitalization in all Text modes, add
 ;; this to your site-lisp/default.el or ~/.emacs file:
-;; 	(add-hook 'text-mode-hook 'turn-on-auto-capitalize-mode)
+;; (add-hook 'text-mode-hook 'turn-on-auto-capitalize-mode)
 ;; To enable (interactive) capitalization in all Text modes, add this
 ;; to your site-lisp/default.el or ~/.emacs file:
-;; 	(add-hook 'text-mode-hook 'enable-auto-capitalize-mode)
+;; (add-hook 'text-mode-hook 'enable-auto-capitalize-mode)
 ;; 
 ;; To prevent a word from ever being capitalized or upcased
 ;; (e.g. "http"), simply add it (in lowercase) to the
@@ -71,8 +71,8 @@
 ;; (e.g. "I'm") in the middle of a sentence in Text mode, define the
 ;; apostrophe as a punctuation character or as a symbol that joins two
 ;; words:
-;; 	;; Use "_" instead of "." to define apostrophe as a symbol:
-;; 	(modify-syntax-entry ?' ".   " text-mode-syntax-table) ; was "w   "
+;; ;; Use "_" instead of "." to define apostrophe as a symbol:
+;; (modify-syntax-entry ?' ".   " text-mode-syntax-table) ; was "w   "
 
 ;;; Code:
 
@@ -114,7 +114,7 @@ see `\\[auto-capitalize-mode]', `\\[turn-on-capitalize-mode]', or
 
 (or (assq 'auto-capitalize minor-mode-alist)
     (setq minor-mode-alist
-	  (cons (list 'auto-capitalize " Cap") minor-mode-alist)))
+          (cons (list 'auto-capitalize " Cap") minor-mode-alist)))
 
 
 (defvar auto-capitalize-yank nil
@@ -126,7 +126,7 @@ see `\\[auto-capitalize-mode]', `\\[turn-on-capitalize-mode]', or
 
 ;; User variables:
 
-(defvar auto-capitalize-words '("I")	;  "Stallman" "GNU" "http"
+(defvar auto-capitalize-words '("I");  "Stallman" "GNU" "http"
   "If non-nil, a list of proper nouns or acronyms.
 If `auto-capitalize' mode is on, these words will be automatically
 capitalized or upcased as listed (mixed case is allowable as well), even
@@ -145,7 +145,7 @@ non-nil value if the current word is within \"normal\" text.")
 
 
 ;; Commands:
-	
+
 (defun auto-capitalize-mode (&optional arg)
   "Toggle `auto-capitalize' minor mode in this buffer.
 With optional prefix ARG, turn `auto-capitalize' mode on iff ARG is positive.
@@ -153,9 +153,9 @@ This sets `auto-capitalize' to t or nil (for this buffer) and ensures that
 `auto-capitalize' is installed in `after-change-functions' (for all buffers)."
   (interactive "P")
   (setq auto-capitalize
-	(if (null arg)
-	    (not auto-capitalize)
-	  (> (prefix-numeric-value arg) 0)))
+        (if (null arg)
+            (not auto-capitalize)
+          (> (prefix-numeric-value arg) 0)))
   (add-hook 'after-change-functions 'auto-capitalize nil t))
 
 (defun turn-on-auto-capitalize-mode ()
@@ -179,7 +179,7 @@ This sets `auto-capitalize' to `query'."
 
 ;; Internal functions:
 
-(defun auto-capitalize-sentence-end()		
+(defun auto-capitalize-sentence-end()
   "portability function. emacs 22.0.50 introduced sentence-end
 function, not available on other emacsen. 
 Fix known to work on 23.0.90 and later"
@@ -202,163 +202,163 @@ Capitalization can be disabled in specific contexts via the
 
 This should be installed as an `after-change-function'."
   (if (and auto-capitalize
-	   (or (null auto-capitalize-predicate)
-	       (funcall auto-capitalize-predicate)))
+           (or (null auto-capitalize-predicate)
+               (funcall auto-capitalize-predicate)))
       (cond ((or (and (or (eq this-command 'self-insert-command)
-			  ;; LaTeX mode binds "." to TeX-insert-punctuation,
-			  ;; and "\"" to TeX-insert-quote:
-			  (let ((key (this-command-keys)))
-			    ;; XEmacs `lookup-key' signals "unable to bind
-			    ;; this type of event" for commands invoked via
-			    ;; the mouse:
-			    (and (if (and (vectorp key)
+                          ;; LaTeX mode binds "." to TeX-insert-punctuation,
+                          ;; and "\"" to TeX-insert-quote:
+                          (let ((key (this-command-keys)))
+                            ;; XEmacs `lookup-key' signals "unable to bind
+                            ;; this type of event" for commands invoked via
+                            ;; the mouse:
+                            (and (if (and (vectorp key)
                                           (> (length key) 0)
-					  (fboundp 'misc-user-event-p)
-					  (misc-user-event-p (aref key 0)))
-				     nil
-				   (eq (lookup-key global-map key t)
-				       'self-insert-command))
-				 ;; single character insertion?
-				 (= length 0)
-				 (= (- end beg) 1))))
-		      (let ((self-insert-char
-			     (cond ((fboundp 'event-to-character) ; XEmacs
-				    (event-to-character last-command-event
-							nil nil t))
-				   (t last-command-event)))) ; GNU Emacs
-			(not (equal (char-syntax self-insert-char) ?w))))
-		 (eq this-command 'newline)
-		 (eq this-command 'newline-and-indent))
-	     ;; self-inserting, non-word character
-	     (if (and (> beg (point-min))
-		      (equal (char-syntax (char-after (1- beg))) ?w))
-		 ;; preceded by a word character
-		 (save-excursion
-		   (forward-word -1)
-		   (save-match-data
-		     (let* ((word-start (point))
-			    (text-start
-			     (progn
-			       (while (or (cl-minusp (skip-chars-backward "\""))
-					  (cl-minusp (skip-syntax-backward "\"(")))
-				 t)
-			       (point)))
-			    lowercase-word)
-		       (cond ((and auto-capitalize-words
-				   (let ((case-fold-search nil))
-				     (goto-char word-start)
-				     (looking-at
-				      (concat "\\("
-					      (mapconcat 'downcase
-							 auto-capitalize-words
-							 "\\|")
-					      "\\)\\>"))))
-			      ;; user-specified capitalization
-			      (if (not (member (setq lowercase-word
-						     (buffer-substring ; -no-properties?
-						      (match-beginning 1)
-						      (match-end 1)))
-					       auto-capitalize-words))
-				  ;; not preserving lower case
-				  (progn ; capitalize!
-				    (undo-boundary)
-				    (replace-match (cl-find lowercase-word
-							 auto-capitalize-words
-							 :key 'downcase
-							 :test 'string-equal)
-						   t t))))
-			     ((and (or (equal text-start (point-min)) ; (bobp)
-				       (progn ; beginning of paragraph?
-					 (goto-char text-start)
-					 (and (= (current-column) left-margin)
-					      (zerop (forward-line -1))
-					      (looking-at paragraph-separate)))
-				       (progn ; beginning of paragraph?
-					 (goto-char text-start)
-					 (and (= (current-column) left-margin)
-					      (re-search-backward paragraph-start
-								  nil t)
-					      (= (match-end 0) text-start)
-					      (= (current-column) left-margin)))
-				       (progn ; beginning of sentence?
-					 (goto-char text-start)
-					 (save-restriction
-					   (narrow-to-region (point-min)
-							     word-start)
-					   (and (re-search-backward (auto-capitalize-sentence-end)
-								    nil t)
-						(= (match-end 0) text-start)
-						;; verify: preceded by
-						;; whitespace?
-						(let ((previous-char
-						       (char-after
-							(1- text-start))))
-						  ;; In some modes, newline
-						  ;; (^J, aka LFD) is comment-
-						  ;; end, not whitespace:
-						  (or (equal previous-char
-							     ?\n)
-						      (equal (char-syntax
-							      previous-char)
-							     ? )))
-						;; verify: not preceded by
-						;; an abbreviation?
-						(let ((case-fold-search nil)
-						      (abbrev-regexp
+                                          (fboundp 'misc-user-event-p)
+                                          (misc-user-event-p (aref key 0)))
+                                     nil
+                                   (eq (lookup-key global-map key t)
+                                       'self-insert-command))
+                                 ;; single character insertion?
+                                 (= length 0)
+                                 (= (- end beg) 1))))
+                      (let ((self-insert-char
+                             (cond ((fboundp 'event-to-character) ; XEmacs
+                                    (event-to-character last-command-event
+                                                        nil nil t))
+                                   (t last-command-event)))) ; GNU Emacs
+                        (not (equal (char-syntax self-insert-char) ?w))))
+                 (eq this-command 'newline)
+                 (eq this-command 'newline-and-indent))
+             ;; self-inserting, non-word character
+             (if (and (> beg (point-min))
+                      (equal (char-syntax (char-after (1- beg))) ?w))
+                 ;; preceded by a word character
+                 (save-excursion
+                   (forward-word -1)
+                   (save-match-data
+                     (let* ((word-start (point))
+                            (text-start
+                             (progn
+                               (while (or (cl-minusp (skip-chars-backward "\""))
+                                          (cl-minusp (skip-syntax-backward "\"(")))
+                                 t)
+                               (point)))
+                            lowercase-word)
+                       (cond ((and auto-capitalize-words
+                                   (let ((case-fold-search nil))
+                                     (goto-char word-start)
+                                     (looking-at
+                                      (concat "\\("
+                                              (mapconcat 'downcase
+                                                         auto-capitalize-words
+                                                         "\\|")
+                                              "\\)\\>"))))
+                              ;; user-specified capitalization
+                              (if (not (member (setq lowercase-word
+                                                     (buffer-substring ; -no-properties?
+                                                      (match-beginning 1)
+                                                      (match-end 1)))
+                                               auto-capitalize-words))
+                                  ;; not preserving lower case
+                                  (progn ; capitalize!
+                                    (undo-boundary)
+                                    (replace-match (cl-find lowercase-word
+                                                            auto-capitalize-words
+                                                            :key 'downcase
+                                                            :test 'string-equal)
+                                                   t t))))
+                             ((and (or (equal text-start (point-min)) ; (bobp)
+                                       (progn ; beginning of paragraph?
+                                         (goto-char text-start)
+                                         (and (= (current-column) left-margin)
+                                              (zerop (forward-line -1))
+                                              (looking-at paragraph-separate)))
+                                       (progn ; beginning of paragraph?
+                                         (goto-char text-start)
+                                         (and (= (current-column) left-margin)
+                                              (re-search-backward paragraph-start
+                                                                  nil t)
+                                              (= (match-end 0) text-start)
+                                              (= (current-column) left-margin)))
+                                       (progn ; beginning of sentence?
+                                         (goto-char text-start)
+                                         (save-restriction
+                                           (narrow-to-region (point-min)
+                                                             word-start)
+                                           (and (re-search-backward (auto-capitalize-sentence-end)
+                                                                    nil t)
+                                                (= (match-end 0) text-start)
+                                                ;; verify: preceded by
+                                                ;; whitespace?
+                                                (let ((previous-char
+                                                       (char-after
+                                                        (1- text-start))))
+                                                  ;; In some modes, newline
+                                                  ;; (^J, aka LFD) is comment-
+                                                  ;; end, not whitespace:
+                                                  (or (equal previous-char
+                                                             ?\n)
+                                                      (equal (char-syntax
+                                                              previous-char)
+                                                             ? )))
+                                                ;; verify: not preceded by
+                                                ;; an abbreviation?
+                                                (let ((case-fold-search nil)
+                                                      (abbrev-regexp
                                                        (if (featurep 'xemacs)
                                                            ;; iso-8859-5, �-��-� �-��-�
                                                            "\\<\\([A-Z�-��-�]?[a-z�-��-�]+\\.\\)+\\="
                                                          "\\<\\([[:upper:]]?[[:lower:]]+\\.\\)+\\=")))
-						  (goto-char
-						   (1+ (match-beginning 0)))
-						  (or (not
-						       (re-search-backward abbrev-regexp
-									   nil t))
-						      (not
-						       (member
-							(buffer-substring ; -no-properties?
-							 (match-beginning 0)
-							 (match-end 0))
-							auto-capitalize-words))))
-						))))
-				   ;; inserting lowercase text?
-				   (let ((case-fold-search nil))
-				     (goto-char word-start)
-				     (looking-at (if (featurep 'xemacs)
+                                                  (goto-char
+                                                   (1+ (match-beginning 0)))
+                                                  (or (not
+                                                       (re-search-backward abbrev-regexp
+                                                                           nil t))
+                                                      (not
+                                                       (member
+                                                        (buffer-substring ; -no-properties?
+                                                         (match-beginning 0)
+                                                         (match-end 0))
+                                                        auto-capitalize-words))))
+                                                ))))
+                                   ;; inserting lowercase text?
+                                   (let ((case-fold-search nil))
+                                     (goto-char word-start)
+                                     (looking-at (if (featurep 'xemacs)
                                                      "[a-z�-��-�]+"
                                                    "[[:lower:]]+")))
-				   (or (eq auto-capitalize t)
-				       (prog1 (y-or-n-p
-					       (format "Capitalize \"%s\"? "
-						       (buffer-substring
-							(match-beginning 0)
-							(match-end 0))))
-					 (message ""))))
-			      ;; capitalize!
-			      (undo-boundary)
-			      (goto-char word-start)
-			      (capitalize-word 1))))))))
-	    ((and auto-capitalize-yank
-		  ;; `yank' sets `this-command' to t, and the
-		  ;; after-change-functions are run before it has been
-		  ;; reset:
-		  (or (eq this-command 'yank)
-		      (and (= length 0) ; insertion?
-			   (eq this-command 't))))
-	     (save-excursion
-	       (goto-char beg)
-	       (save-match-data
-		 (while (re-search-forward "\\Sw" end t)
-		   ;; recursion!
-		   (let* ((this-command 'self-insert-command)
-			  (non-word-char (char-after (match-beginning 0)))
-			  (last-command-event
-			   (cond ((fboundp 'character-to-event) ; XEmacs
-				  (character-to-event non-word-char))
-				 (t non-word-char)))) ; GNU Emacs
-		     (auto-capitalize (match-beginning 0)
-				      (match-end 0)
-				      0)))))))))
+                                   (or (eq auto-capitalize t)
+                                       (prog1 (y-or-n-p
+                                               (format "Capitalize \"%s\"? "
+                                                       (buffer-substring
+                                                        (match-beginning 0)
+                                                        (match-end 0))))
+                                         (message ""))))
+                              ;; capitalize!
+                              (undo-boundary)
+                              (goto-char word-start)
+                              (capitalize-word 1))))))))
+            ((and auto-capitalize-yank
+                  ;; `yank' sets `this-command' to t, and the
+                  ;; after-change-functions are run before it has been
+                  ;; reset:
+                  (or (eq this-command 'yank)
+                      (and (= length 0) ; insertion?
+                           (eq this-command 't))))
+             (save-excursion
+               (goto-char beg)
+               (save-match-data
+                 (while (re-search-forward "\\Sw" end t)
+                   ;; recursion!
+                   (let* ((this-command 'self-insert-command)
+                          (non-word-char (char-after (match-beginning 0)))
+                          (last-command-event
+                           (cond ((fboundp 'character-to-event) ; XEmacs
+                                  (character-to-event non-word-char))
+                                 (t non-word-char)))) ; GNU Emacs
+                     (auto-capitalize (match-beginning 0)
+                                      (match-end 0)
+                                      0)))))))))
 
 ;;; auto-capitalize.el ends here
 
